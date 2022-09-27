b8c991297f9affebe36aa15dc315afa23cbc89bf
Abhishek Singh <46836258+abhishekwayne@users.noreply.github.com>
2022-09-28 01:19:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
